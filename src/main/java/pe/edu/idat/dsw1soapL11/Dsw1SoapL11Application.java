20a5a4d189e3116383561907f202214d511adbdf
Ethel <ethelcruz602@gmail.com>
2023-06-05 12:56:38 -0500

    primercommit

---
+++ src/main/java/pe/edu/idat/dsw1soapL11/Dsw1SoapL11Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1soapL11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1SoapL11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1SoapL11Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/dsw1soapL11/Dsw1SoapL11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1soapL11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1SoapL11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
